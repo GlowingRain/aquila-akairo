998e0e7e205cba196bbe6cedeb6fee5ec505b1cc
xem <xem.discord.gg@gmail.com>
2019-02-23 23:47:06 -0300

    canvas testing

--- src/commands/Miscelaneo/spotify.js
+++ src/commands/Miscelaneo/spotify.js
@@ -2,50 +2,59 @@ const { Command } = require('discord-akairo');
 const { RichEmbed } = require('discord.js');
 const { errorMessage, warnMessage } = require('../../modules/errors');
 const { green } = require('../../modules/colors');
+const Canvas = require('canvas');
 
 class SpotifyCommand extends Command {
     constructor() {
         super('spotify', {
             aliases: ['spotify'],
             channelRestriction: 'guild',
-
+            args: [
+                {
+                    id: 'beta',
+                    match: 'flag',
+                    prefix: '--beta'
+                }
+            ]
         });
     }
 
-    exec(message) {
+    async exec(message, args) {
 
         let user = message.mentions.users.first() || message.author;
 
+        let trackImg = user.presence.game.assets.largeImageURL;
+        let trackUrl = `https://open.spotify.com/track/${user.presence.game.syncID}`;
+        let trackName = user.presence.game.details;
+        let trackAlbum = user.presence.game.assets.largeText;
+        let trackAuthor = user.presence.game.state;
+        
         if (!user.presence.game) return;
 
         if (user.presence.game.name === 'Spotify' && user.presence.game.type === 2) {
-
-            try {
-                let trackImg = user.presence.game.assets.largeImageURL;
-                let trackUrl = `https://open.spotify.com/track/${user.presence.game.syncID}`;
-                let trackName = user.presence.game.details;
-                let trackAlbum = user.presence.game.assets.largeText;
-                let trackAuthor = user.presence.game.state;
-
                 const embed = new RichEmbed()
                     .setAuthor(`Spotify | ${user.username}#${user.discriminator}`, 'https://www.iosicongallery.com/icons/spotify-music-2015-07-30/512.png')
                     .setColor(green)
                     .setThumbnail(trackImg)
-                    .setDescription(`
-                        \`🎵\` **Nombre de la canción :**  \`${trackName}\`
-                        \n\`📀\` **Álbum :**  \`${trackAlbum}\`
-                        \n\`🎤\` **Autor(es) :**  \`${trackAuthor}\`
-                        
-                        \n\`🔊\` **Escucha esta canción en [Spotify](${trackUrl})**
-                        `);
-                // .addField('Escucha esta canción', `[${trackUrl}](${trackUrl})`, false);
-
-                return message.channel.send(embed);
-
-            } catch (error) {
-                return warnMessage('Puede ser que se esté reproduciendo una canción sin registrar.', message);
-            };
+                    .setDescription(`\`🎵\` | Nombre - **\`${trackName}\`**\n` +
+                        `\`🎤\` | Autor(es) - **\`${trackAuthor}\`**\n` +
+                        `\`📀\` | Álbum - **\`${trackAlbum}\`**\n\n` +
+                        `\`🔊\` **Escucha esta canción en [Spotify](${trackUrl})**    
+                    `);
         };
+
+        if (args.beta) {
+            const canvas = Canvas.createCanvas(700, 250);
+            const ctx = canvas.getContext('2d');
+
+            const background = await Canvas.loadImage('/src/stuff/bg.jpg');
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+            const attachment = new Discord.Attachment(canvas.toBuffer(), 'bg.jpg');
+
+            return message.channel.send(attachment);
+        }
+
+        return message.channel.send(embed);
     }
 }
 
