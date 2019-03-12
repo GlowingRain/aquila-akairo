be25ffb59288521c973ff2d22e235c9fc1178b9b
xem <xem.discord.gg@gmail.com>
2019-03-12 19:27:37 -0300

    removed things

--- src/listeners/commandLoaded.js
+++ src/listeners/commandLoaded.js
@@ -16,12 +16,9 @@ class CommandLoadedListener extends Listener {
         let timestamp = `${moment(new Date()).format("DD-MM-YY HH:mm:ss")}`;
 
         // Create the log itself with Chalk and Moment
-        let text = `Cargando el comando ${chalk.magenta.bold(commandLoaded)}\n`
-        const spinner = ora(text, 'dots');
-        spinner.start();
+        let text = `${timestamp} | Cargando el comando ${chalk.magenta.bold(commandLoaded)}\n`
+        const spinner = ora(text).start();
         setTimeout(() => {
-            spinner.color = 'yellow';
-            spinner.text = 'Loading rainbows';
             spinner.succeed(`${timestamp} | ${chalk.green.bold('Todos los comandos fueron cargados')}`)
         });
     }
