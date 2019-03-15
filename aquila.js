a59eaf14e9802701b0aed5f167794b0390e7f2a3
xem <xem.discord.gg@gmail.com>
2019-03-15 16:35:46 -0300

    unhandledRejection listener added

--- aquila.js
+++ aquila.js
@@ -2,12 +2,23 @@ const { AkairoClient } = require('discord-akairo');
 require('dotenv').config();
 
 const client = new AkairoClient({
+    // Settings
     ownerID: process.env.OWNER_ID,
     prefix: process.env.PREFIX,
+    
+    // Emiters
+    emitters: {
+        process
+    },
+
+    // Directories
     commandDirectory: './src/commands/',
     listenerDirectory: './src/listeners/',
+
+    // CommandRelated
     automateCategories: true,
     commandUtil: true,
+    
 }, { disableEveryone: true });
 
 client.login();
