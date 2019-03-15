cfdc2f3616cd249bb33c54d71db732998b0e83c7
xem <xem.discord.gg@gmail.com>
2019-03-15 16:12:24 -0300

    error log

---
+++ src/listeners/commandError.js
@@ -0,0 +1,23 @@
+const { Listener } = require('discord-akairo');
+const chalk = require('chalk');
+const moment = require('moment');
+
+class CommandErrorListener extends Listener {
+    constructor() {
+        super('commandError', {
+            emitter: 'commandHandler',
+            eventName: 'error'
+        })
+    }
+
+    exec(command, error) {
+        let timestamp = `${moment(new Date()).format("DD-MM-YY HH:mm:ss")}`;
+
+        // Create the log itself with Chalk and Moment
+        let log = `${timestamp} | ${chalk.bgRed.bold(`ERROR - ${error}`)} | ${command}`
+
+        console.log(log);
+    }
+}
+
+module.exports = CommandErrorListener;
